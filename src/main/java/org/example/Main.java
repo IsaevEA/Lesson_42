54cf50e0478ee79c0690a3c2f14dc42168b28b9c
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-09-05 19:44:49 +0600

    вторрая ветка

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int a = 10;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
